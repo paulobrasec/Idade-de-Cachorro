105eecd06a21a1025c1200e54711ce04ff725cf0
Paulo Santos <paulobrasec@gmail.com>
2018-07-19 21:37:20 -0300

    Initial Commit

--- Idade de Cachorro/ViewController.swift
+++ Idade de Cachorro/ViewController.swift
@@ -9,6 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var legendaResultado: UILabel!
+    @IBOutlet weak var campoIdadeCachorro: UITextField!
+    
+    @IBAction func descobrirIdade(_ sender: Any) {
+    
+        let idade = Int(campoIdadeCachorro.text!)! * 7
+        legendaResultado.text = "A idade do cachorro é: " + String(idade)
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
